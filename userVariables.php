fb0c1a83fbcb64acac5cf88d4521c246d1cf01ec
StarvingPoet <starvingpoet@clan-quant.net>
2018-06-20 10:08:34 -0500

    new use variable

--- userVariables.php
+++ userVariables.php
@@ -13,7 +13,8 @@ $user     = 'user/.XCF/';                       //User Folder - where mods and s
 $saves    = 'xcom1/';                           //Save Folder
 
 /** The following is for making your BattleStats visible online **/
-$options['broadcast']   = true;                                       // true or false - if true, supply below
-$options['displayName'] = "DisplayName";                              // What's your handle?
-$options['passKey']     = "randomwordsgohereandknockyourse";          // Just pick a couple random words, not cryptographically secure - DONT USE A PASSWORD
+  $options['broadcast']   = true;                              // true or false - if true, supply below
+  $options['displayName'] = "Display Name";                    // What's your handle?
+  $options['passKey']     = "RandomWordsGoHere";               // Just pick a couple random words, not cryptographically secure - DONT USE A PASSWORD
+$options['defaultNames'] = array('Case Officer', 'Al', 'K9') // Used in conjunction with the clone function to determine which soldiers are named or not.  Soldiers with these names will not be cloned and will be pulled from training as needed.  Highly recommended that you use a naming mod to change the default names
 ?>
